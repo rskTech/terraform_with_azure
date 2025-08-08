22603df83080178d37b166e1f6ca3919596a051b
Rajendra <rajendrait99@gmail.com>
2025-08-08 08:09:25 +0530

    Update and rename main.tf to resources.tf

--- azure_network_security_group/resources.tf
+++ azure_network_security_group/resources.tf
@@ -1,25 +1,3 @@
-provider "azurerm" {
-  features {} # Required for the AzureRM provider
-}
-
-variable "resource_group_name" {
-  description = "The name of the Azure Resource Group to create for the NSG."
-  type        = string
-  default     = "my-azure-nsg-rg" # You can change this name
-}
-
-variable "location" {
-  description = "The Azure region where the NSG will be deployed (e.g., East US, West Europe, Central India)."
-  type        = string
-  default     = "East US" # Choose an Azure region available in your subscription
-}
-
-variable "nsg_name" {
-  description = "The name of the Network Security Group to create."
-  type        = string
-  default     = "my-application-nsg" # You can change this name
-}
-
 # --- Azure Resource Creation ---
 
 # 1. Azure Resource Group
